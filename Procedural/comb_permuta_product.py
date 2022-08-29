28155d8bfdcc98feec2b750f4c707a5eb2f90602
Figueredo <jhowfigueredo2011@hotmail.com>
2022-08-29 17:17:11 -0300

    Create comb_permuta_product.py

---
+++ Procedural/comb_permuta_product.py
@@ -0,0 +1,10 @@
+"""
+Combinations, Permutations e Product - Itertools
+Combinação - Ordem não importa
+Permutação - Ordem importa
+Ambos não repetem valores únicos
+
+Produto - Ordem importa e repete valores únicos
+"""
+
+pessoa = ['Luiz', 'Andre', 'Maria', 'Pedro', 'Adriana']
